4de106eee0ffa79b459c43cb8398b5527fc419c8
Owen McGrath <owenmcgrath85@gmail.com>
2018-07-01 23:32:18 -0400

    Loops every 10 seconds now, fixed printStandings()

--- dash.py
+++ dash.py
@@ -6,6 +6,9 @@ from Twitter_API_Key_Info import *
 
 import datetime
 
+import threading
+
+import os
 
 api = twitter.Api(consumer_key = consumer_key,
 				  consumer_secret = consumer_secret,
@@ -23,9 +26,9 @@ def getTweet(handle):
 	tweets = api.GetUserTimeline(screen_name = handle)
 	return tweets[0].text
 
-def printStandings(teams):
+def printStandings(div):
 	print("\nSTANDINGS\n=========\n")
-	for t in teams:
+	for t in div.teams:
 		print(t.team_abbrev + " " + str(t.w) + " " + str(t.l) + " " + str(t.gb))
 
 def printUpcomingGame(game):
@@ -88,33 +91,43 @@ def printTweets():
 	print("MLB (@mlb): " + getTweet("mlb"))
 	print("Red Sox (@alexspeier): " + getTweet("RedSox"))
 
+def looper():
+	os.system('cls')
+	threading.Timer(10.0, looper).start()
+	gameday = datetime.datetime.now()
+	# tomorrow = gameday - datetime.timedelta(1)
 
-#GETTING DATA FROM MLB
-today = datetime.datetime.now()
-# tomorrow = today - datetime.timedelta(1)
-game = mlbgame.day(today.year, today.month, today.day, home='Red Sox', away = 'Red Sox')[0]
+	noGameFound = True
 
-standings = mlbgame.standings()
-alEast = standings.divisions[3]
-
-#game is over, display final score
-if(game.game_status == "FINAL"):
-	printStandings(alEast)
-	printFinalScore(game)
-	printTweets()
-
-#display upcoming game info + standings 
-elif(game.game_status == "PRE_GAME"):
-	printStandings(alEast)
-	printUpcomingGame(game)
-	printTweets()
-
-#game is on, display live game info
-else:
-	printCurrentGame(game)
+	while noGameFound:
+			#check for game gameday
+		if len(mlbgame.day(gameday.year, gameday.month, gameday.day, home='Red Sox', away = 'Red Sox')) != 0:
+			game = mlbgame.day(gameday.year, gameday.month, gameday.day, home='Red Sox', away = 'Red Sox')[0]
+			noGameFound = False
+			#if no game on the current day check on the next day
+		else:
+			gameday += datatime.timedelta(1)
 
 
 
-# print("SOX GAME INFO\n=============\n")
+	standings = mlbgame.standings()
+	alEast = standings.divisions[3]
 
+	#game is over, display final score
+	if(game.game_status == "FINAL"):
+		printStandings(alEast)
+		printFinalScore(game)
+		printTweets()
+
+	#display upcoming game info + standings 
+	elif(game.game_status == "PRE_GAME"):
+		printStandings(alEast)
+		printUpcomingGame(game)
+		printTweets()
+
+	#game is on, display live game info
+	else:
+		printCurrentGame(game)
+
+looper()
 
